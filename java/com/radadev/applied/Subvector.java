50a893335bd23fd5b1a5812cce86ed5ebf65eefa
Alex Avance <aravance@gmail.com>
2013-06-22 21:59:53 -0500

    Reduce allocation and garbage collection cost in subvector.

--- java/com/radadev/applied/Subvector.java
+++ java/com/radadev/applied/Subvector.java
@@ -1,29 +1,30 @@
 package  com.radadev.applied;
 
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Subvector extends AppliedAlgorithm {
 
-    private long maxsub(int[] array) {
-        long[] sums = new long[array.length];
-        long max = sums[0] = array[0];
-        for (int i = 1; i < sums.length; ++i) {
-            sums[i] = array[i];
-            if (sums[i - 1] > 0) sums[i] += sums[i - 1];
-            if (sums[i] > max) max = sums[i];
+    private long maxsub(ArrayList<Long> array) {
+        long max = array.get(0);
+        for (int i = 1; i < array.size(); ++i) {
+            if (array.get(i - 1) > 0) array.set(i, array.get(i) + array.get(i - 1));
+            if (array.get(i) > max) max = array.get(i);
         }
         return max;
     }
 
     @Override
     void execute(Scanner in, PrintStream out) {
+        ArrayList<Long> array = new ArrayList<>(1000000);
         final int count = in.nextInt();
         for (int i = 0; i < count; ++i) {
+            array.clear();
             int size = in.nextInt();
-            int array[] = new int[size];
+            array.ensureCapacity(size);
             for (int j = 0; j < size; ++j) {
-                array[j] = in.nextInt();
+                array.add(in.nextLong());
             }
             out.println(maxsub(array));
         }
